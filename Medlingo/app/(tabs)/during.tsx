fa964731aca3ad9ad760d9f67c97ec582c1abbcb
Dave <davesprogrammingcs@gmail.com>
2025-11-03 16:14:54 -0600

    - created base styles and containers

--- Medlingo/app/(tabs)/during.tsx
+++ Medlingo/app/(tabs)/during.tsx
@@ -1,10 +1,29 @@
 // app/(tabs)/settings.tsx
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function SettingsScreen() {
     return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings Screen</Text>
+    <View style={styles.container}>
+        <Text style={styles.button}>Settings Screen</Text>
+        <Text style={styles.button}>Settings Screen</Text>
+        <Text style={styles.button}>Settings Screen</Text>
+        <Text style={styles.button}>Settings Screen</Text>
+        <Text style={styles.button}>Settings Screen</Text>
+        <Text style={styles.button}>Settings Screen</Text>
     </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20
+    },
+    button: {
+        borderRadius: 8,
+        borderColor: 'black',
+        borderWidth: 2
+    }
+})

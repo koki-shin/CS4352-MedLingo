1d5ba399e23efd4f29ac9868389e64c1a8ab6c76
Tanner <tan19man20@gmail.com>
2025-11-10 12:49:09 -0600

    fix brackets

--- Medlingo/app/(tabs)/_layout.tsx
+++ Medlingo/app/(tabs)/_layout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { LanguageProvider } from "../../hooks/LanguageContext"; 
-
 export default function TabLayout() {
   return (
     <LanguageProvider>
@@ -17,29 +16,16 @@ export default function TabLayout() {
             title: "home",
             tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
           }}
-        >
-          <Tabs.Screen
-            name="home" // Corresponds to app/(tabs)/home.tsx
-            options={{
-              title: 'Home',
-              tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
-            }}
-          />
-          <Tabs.Screen
-            name="before" // Corresponds to app/(tabs)/settings.tsx
-            options={{
-              title: 'Before Appointment',
-              tabBarIcon: ({ color }) => <Ionicons name="push" size={24} color={color} />,
-            }}
-          />
-           <Tabs.Screen
-            name="during" // Corresponds to app/(tabs)/settings.tsx
-            options={{
-              title: 'During Appointment',
-              tabBarIcon: ({ color }) => <Ionicons name="push" size={24} color={color} />,
-            }}
-          />
-          {/* Add more Tabs.Screen components for each tab */}
-        </Tabs>
-      );
-    }
+        />
+        <Tabs.Screen
+          name="before" 
+          options={{
+            title: "Before Appointment",
+            tabBarIcon: ({ color }) => <Ionicons name="push" size={24} color={color} />,
+          }}
+        />
+        {/* Add more Tabs*/}
+      </Tabs>
+    </LanguageProvider>
+  );
+}

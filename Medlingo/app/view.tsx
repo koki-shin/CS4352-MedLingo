662b648c33efcc2c1eb6a7e5a9f757e698a60ff3
tanner <tan19man20@gmail.com>
2025-12-07 14:31:21 -0600

    viewer back button

--- Medlingo/app/view.tsx
+++ Medlingo/app/view.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { View, ActivityIndicator, Text, Button } from "react-native";
-import { WebView } from "react-native-webview";
+import { View, ActivityIndicator, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
+import { WebView } from "react-native-webview";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ViewPDF() {
@@ -27,7 +27,7 @@ export default function ViewPDF() {
       </Text>
 
       {/* pdf */}
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "white" }}>
         <WebView
           style={{ flex: 1 }}
           originWhitelist={["*"]}
@@ -41,10 +41,39 @@ export default function ViewPDF() {
         />
       </View>
 
-      {/* return */}
       <View className="p-4">
-        <Button title= "Back" onPress={() => router.back() }/>
+        {/* End Session */}
+        <TouchableOpacity
+          style={styles.bottom_button}
+          onPress={() => router.back() }
+        >
+          <Text style={styles.button_text}>
+            Return
+          </Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+    bottom_button: {
+    alignSelf: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0A4DA3',
+    borderRadius: 22,
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    marginTop: 8,
+    borderWidth: 2,
+    borderColor: '#000000ff',
+  },
+  button_text: {
+    color: '#FFFFFF',
+    fontWeight: '700',
+    fontSize: 16,
+    fontFamily: 'Montserrat-Bold',
+  }
+});
